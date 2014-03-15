a8819a510b919be43ff3011b904f257798fb8916
Honza Král <honza.kral@gmail.com>
2014-03-15 17:02:21 +0100

    Simpler run_tests.py

--- test_elasticsearch_dsl/run_tests.py
+++ test_elasticsearch_dsl/run_tests.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
 import sys
 
 import pytest
 
 def run_all(argv=None):
-    sys.exitfunc = lambda: sys.stderr.write('Shutting down....\n')
-
     # always insert coverage when running tests through setup.py
     if argv is None:
-        argv = [ '--cov', 'elasticsearch_dsl', '--verbose', '--junitxml', 'junit.xml', '--cov-report', 'xml']
+        argv = ['--cov', 'elasticsearch_dsl', '--verbose', '--junitxml', 'junit.xml', '--cov-report', 'xml']
     else:
         argv = argv[1:]
 
-    sys.exit( pytest.main(argv))
+    sys.exit(pytest.main(argv))
 
 if __name__ == '__main__':
     run_all(sys.argv)
