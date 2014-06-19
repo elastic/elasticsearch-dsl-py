5924f2b0243a41636234713d968672ee76aa2a67
Honza Král <honza.kral@gmail.com>
2014-06-19 15:54:51 +0200

    Make dummy_response fixture availible

--- test_elasticsearch_dsl/conftest.py
+++ test_elasticsearch_dsl/conftest.py
@@ -32,3 +32,64 @@ def data_client(request, client):
     request.addfinalizer(lambda: client.indices.delete('git'))
     return client
 
+@fixture
+def dummy_response():
+    return {
+      "_shards": {
+        "failed": 0,
+        "successful": 10,
+        "total": 10
+      },
+      "hits": {
+        "hits": [
+          {
+            "_index": "test-index",
+            "_type": "company",
+            "_id": "elasticsearch",
+            "_score": 12.0,
+
+            "_source": {
+              "city": "Amsterdam",
+              "name": "Elasticsearch",
+            },
+          },
+          {
+            "_index": "test-index",
+            "_type": "employee",
+            "_id": "42",
+            "_score": 11.123,
+            "_parent": "elasticsearch",
+
+            "_source": {
+              "name": {
+                "first": "Shay",
+                "last": "Bannon"
+              },
+              "lang": "java",
+              "twitter": "kimchy",
+            },
+          },
+          {
+            "_index": "test-index",
+            "_type": "employee",
+            "_id": "47",
+            "_score": 1,
+            "_parent": "elasticsearch",
+
+            "_source": {
+              "name": {
+                "first": "Honza",
+                "last": "Král"
+              },
+              "lang": "python",
+              "twitter": "honzakral",
+            },
+          },
+        ],
+        "max_score": 12.0,
+        "total": 123
+      },
+      "timed_out": False,
+      "took": 123
+    }
+
