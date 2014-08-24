80b640329c01148fcab5fb44c6aaa883291955bc
Honza Král <honza.kral@gmail.com>
2014-08-24 15:09:48 -0700

    Make filter generation dynamic as well, include all the filters

--- elasticsearch_dsl/filter.py
+++ elasticsearch_dsl/filter.py
@@ -1,11 +1,15 @@
 from six import add_metaclass
 
-from .utils import DslMeta, DslBase, BoolMixin
+from .utils import DslMeta, DslBase, BoolMixin, _make_dsl_class
 
 class FilterMeta(DslMeta):
     _classes = {}
 
-def F(name_or_filter, **params):
+def F(name_or_filter, filters=None, **params):
+    # 'and/or', [F(), F()]
+    if filters is not None:
+        params['filters'] = filters
+
     # {"term": {...}}
     if isinstance(name_or_filter, dict):
         if params:
@@ -31,29 +35,6 @@ class Filter(DslBase):
     _type_shortcut = staticmethod(F)
     name = None
 
-class Term(Filter):
-    name = 'term'
-
-class Terms(Filter):
-    name = 'terms'
-
-class Range(Filter):
-    name = 'range'
-
-class Missing(Filter):
-    name = 'missing'
-
-class Bool(BoolMixin, Filter):
-    name = 'bool'
-    _param_defs = {
-        'must': {'type': 'filter', 'multi': True},
-        'should': {'type': 'filter', 'multi': True},
-        'must_not': {'type': 'filter', 'multi': True},
-    }
-
-# register this as Bool for Filter
-Filter._bool = Bool
-
 class MatchAll(Filter):
     name = 'match_all'
     def __add__(self, other):
@@ -63,5 +44,62 @@ class MatchAll(Filter):
     def __or__(self, other):
         return self
     __ror__ = __or__
-
 EMPTY_FILTER = MatchAll()
+
+class Bool(BoolMixin, Filter):
+    name = 'bool'
+    _param_defs = {
+        'must': {'type': 'filter', 'multi': True},
+        'should': {'type': 'filter', 'multi': True},
+        'must_not': {'type': 'filter', 'multi': True},
+    }
+# register this as Bool for Filter
+Filter._bool = Bool
+
+class Not(Filter):
+    name = 'not'
+    _param_defs = {'filter': {'type': 'filter'}}
+
+    def __init__(self, **kwargs):
+        if 'filter' not in kwargs:
+            kwargs = {'filter': kwargs}
+        super(Not, self).__init__(**kwargs)
+
+FILTERS = (
+    # compound filters
+    ('and', {'filters': {'type': 'filter', 'multi': True}}),
+    ('or', {'filters': {'type': 'filter', 'multi': True}}),
+
+    # relationships
+    ('nested', {'filter': {'type': 'filter'}}),
+    ('has_child', {'filter': {'type': 'filter'}}),
+    ('has_parent', {'filter': {'type': 'filter'}}),
+
+    ('query', {'query': {'type': 'query'}}),
+
+    # core filters
+    ('exists', None),
+    ('geo_bounding_box', None),
+    ('geo_distance', None),
+    ('geo_distance_range', None),
+    ('geo_polygon', None),
+    ('geo_shape', None),
+    ('geohash_cell', None),
+    ('ids', None),
+    ('indices', None),
+    ('limit', None),
+    ('missing', None),
+    ('prefix', None),
+    ('range', None),
+    ('regexp', None),
+    ('script', None),
+    ('term', None),
+    ('terms', None),
+    ('type', None),
+)
+
+# generate the filter classes dynamicaly
+for fname, params_def in FILTERS:
+    fclass = _make_dsl_class(Filter, fname, params_def)
+    globals()[fclass.__name__] = fclass
+
