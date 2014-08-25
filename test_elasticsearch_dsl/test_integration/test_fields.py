cfdb9431dc4e01329da498cbd6188002e9172b53
Honza Král <honza.kral@gmail.com>
2014-08-25 14:37:56 -0700

    Add integration test for .fileds()

---
+++ test_elasticsearch_dsl/test_integration/test_fields.py
@@ -0,0 +1,11 @@
+from elasticsearch_dsl.search import Search
+
+def test_search_can_be_limited_to_fields(data_client):
+    s = Search(using=data_client).index('git').doc_type('repos').fields('organization')
+    print(s.to_dict())
+
+    response = s.execute()
+
+    assert response.hits.total == 1
+    assert response.hits[0] == {'organization': ['elasticsearch']}
+
