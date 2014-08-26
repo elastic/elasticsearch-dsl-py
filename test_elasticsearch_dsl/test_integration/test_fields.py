fe2a37a3b3fda10678806054b77ba50bc8d9511b
Honza Král <honza.kral@gmail.com>
2014-08-26 14:03:18 -0700

    Leftover print.
    Thanks robhudson!

--- test_elasticsearch_dsl/test_integration/test_fields.py
+++ test_elasticsearch_dsl/test_integration/test_fields.py
@@ -2,8 +2,6 @@ from elasticsearch_dsl.search import Search
 
 def test_search_can_be_limited_to_fields(data_client):
     s = Search(using=data_client).index('git').doc_type('repos').fields('organization')
-    print(s.to_dict())
-
     response = s.execute()
 
     assert response.hits.total == 1
