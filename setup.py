5b9e19b15bdd7f2add603f8eb72454e8aaccefe1
Honza Král <honza.kral@gmail.com>
2014-08-27 12:22:49 -0700

    Bump version for initial release

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
