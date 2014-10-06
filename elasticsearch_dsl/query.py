e688ce8c588ab843cd72605f77217c2b17a034e0
Andrew Phillipo <andrewphillipo@yahoo.co.uk>
2014-10-06 22:55:35 +0100

    Common Terms should be renamed Common as in the documentation
    See the docs here:
    
    http://www.elasticsearch.org/guide/en/elasticsearch/reference/1.x/query-dsl-common-terms-query.html#query-dsl-common-terms-query
    
    Thanks,
    
    Andy.

--- elasticsearch_dsl/query.py
+++ elasticsearch_dsl/query.py
@@ -87,7 +87,7 @@ QUERIES = (
     ('span_or', {'clauses': {'type': 'query', 'multi': True}}),
 
     # core queries
-    ('common_terms', None),
+    ('common', None),
     ('fuzzy', None),
     ('fuzzy_like_this', None),
     ('fuzzy_like_this_field', None),
