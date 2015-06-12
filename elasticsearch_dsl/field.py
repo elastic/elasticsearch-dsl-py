e4fcf471348b127c0aa5bf98e2fae8ff2137f6b6
Håkan Waara <hwaara@gmail.com>
2015-06-12 18:50:36 +0200

    allow a blank date if that has been specified on the date field

--- elasticsearch_dsl/field.py
+++ elasticsearch_dsl/field.py
@@ -157,6 +157,9 @@ class Date(Field):
             return data
 
         try:
+            if data is None and self._allow_blank:
+                return
+
             # TODO: add format awareness
             return parser.parse(data)
         except Exception as e:
