3e041548d96b8cb5a47163531840c89a658aea82
Gabor Nagy <mail@aigeruth.hu>
2016-04-10 18:49:08 +0100

    Add custom validation method to Boolean field type.
    Validation failed for boolean fields when `required` was set to `True` and
    the field value was `False`.

--- test_elasticsearch_dsl/test_validation.py
+++ test_elasticsearch_dsl/test_validation.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from elasticsearch_dsl import DocType, Nested, String, Date, Object
+from elasticsearch_dsl import DocType, Nested, String, Date, Object, Boolean
 from elasticsearch_dsl.field import InnerObjectWrapper
 from elasticsearch_dsl.exceptions import ValidationException
 
@@ -22,6 +22,11 @@ class BlogPost(DocType):
     )
     created = Date()
     inner = Object()
+
+
+class BlogPostWithStatus(DocType):
+    published = Boolean(required=True)
+
 
 class AutoNowDate(Date):
     def clean(self, data):
@@ -59,6 +64,16 @@ def test_missing_required_field_raises_validation_exception():
     d.authors.append({'name': 'Honza', 'email': 'honza@elastic.co'})
     d.full_clean()
 
+def test_boolean_doesnt_treat_false_as_empty():
+    d = BlogPostWithStatus()
+    with raises(ValidationException):
+        d.full_clean()
+    d.published = False
+    d.full_clean()
+    d.published = True
+    d.full_clean()
+
+
 def test_custom_validation_on_nested_gets_run():
     d = BlogPost(authors=[{'name': 'Honza', 'email': 'king@example.com'}], created=None)
 
@@ -74,7 +89,7 @@ def test_accessing_known_fields_returns_empty_value():
 
     d.authors.append({})
     assert '' == d.authors[0].name
-    assert '' == d.authors[0].email 
+    assert '' == d.authors[0].email
 
 def test_empty_values_are_not_serialized():
     d = BlogPost(authors=[{'name': 'Honza', 'email': 'honza@elastic.co'}], created=None)
