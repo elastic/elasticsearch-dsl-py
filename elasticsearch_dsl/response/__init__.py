f0700563a0654b1c0755f154d78395f7b65b3c34
Honza Král <honza.kral@gmail.com>
2016-09-03 18:56:46 +0200

    separate response and result code

--- elasticsearch_dsl/response/__init__.py
+++ elasticsearch_dsl/response/__init__.py
@@ -1,6 +1,6 @@
-from six import iteritems, u
-
 from ..utils import AttrDict, AttrList
+
+from .result import Result, ResultMeta
 
 class SuggestResponse(AttrDict):
     def success(self):
@@ -53,35 +53,3 @@ class Response(AttrDict):
             for k in h:
                 setattr(self._hits, k, h[k])
         return self._hits
-
-
-class ResultMeta(AttrDict):
-    def __init__(self, document, exclude=('_source', '_fields')):
-        d = dict((k[1:] if k.startswith('_') else k, v) for (k, v) in iteritems(document) if k not in exclude)
-        if 'type' in d:
-            # make sure we are consistent everywhere in python
-            d['doc_type'] = d.pop('type')
-        super(ResultMeta, self).__init__(d)
-
-class Result(AttrDict):
-    def __init__(self, document):
-        data = {}
-        if '_source' in document:
-            data = document['_source']
-        if 'fields' in document:
-            data.update(document['fields'])
-
-        super(Result, self).__init__(data)
-        # assign meta as attribute and not as key in self._d_
-        super(AttrDict, self).__setattr__('meta', ResultMeta(document))
-
-    def __dir__(self):
-        # be sure to expose meta in dir(self)
-        return super(Result, self).__dir__() + ['meta']
-
-    def __repr__(self):
-        return '<Result(%s): %s>' % (
-            '/'.join(getattr(self.meta, key) for key in
-                      ('index', 'doc_type', 'id') if key in self.meta),
-            super(Result, self).__repr__()
-        )
