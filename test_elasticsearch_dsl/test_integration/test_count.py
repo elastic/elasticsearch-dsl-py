384f1d5aadf65814ed529e79dce532f85746baa6
Honza Král <honza.kral@gmail.com>
2016-10-25 19:28:24 +0200

    search_type=count is removed in es 5.0

--- test_elasticsearch_dsl/test_integration/test_count.py
+++ test_elasticsearch_dsl/test_integration/test_count.py
@@ -12,13 +12,3 @@ def test_count_filter(data_client):
     s = Search(using=data_client).index('git').filter(~Q('exists', field='parent_shas'))
     # initial commit + repo document
     assert 2 == s.count()
-
-def test_search_type_count(data_client):
-    s = Search(using=data_client, index='git')
-    s.aggs.bucket('per_type', 'terms', field='_type')
-    s = s.params(search_type='count')
-    result = s.execute()
-
-    assert [] == result.hits
-    assert 2 == len(result.aggregations.per_type.buckets)
-
