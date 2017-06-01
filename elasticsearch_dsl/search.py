0da6869e910676b200a11926c0ac5f5ab0e86774
Honza Král <honza.kral@gmail.com>
2017-06-01 14:10:06 +0200

    Specify highlight options behavior
    Fixes #634

--- elasticsearch_dsl/search.py
+++ elasticsearch_dsl/search.py
@@ -489,7 +489,7 @@ class Search(Request):
     def highlight(self, *fields, **kwargs):
         """
         Request highlighting of some fields. All keyword arguments passed in will be
-        used as parameters. Example::
+        used as parameters for all the fields in the ``fields`` parameter. Example::
 
             Search().highlight('title', 'body', fragment_size=50)
 
@@ -499,6 +499,20 @@ class Search(Request):
                 "highlight": {
                     "fields": {
                         "body": {"fragment_size": 50},
+                        "title": {"fragment_size": 50}
+                    }
+                }
+            }
+
+        If you want to have different options for different fields you can call ``highlight`` twice::
+
+            Search().highlight('title', fragment_size=50).highlight('body', fragment_size=100)
+
+        which will produce::
+            {
+                "highlight": {
+                    "fields": {
+                        "body": {"fragment_size": 100},
                         "title": {"fragment_size": 50}
                     }
                 }
