63b353ffff75ba37396ef56c489b1c5b68afced4
Petr Vacha <pnd@mayapurmedia.com>
2017-07-04 16:11:21 +0200

    Set key to 0 when it is None in DateHistogramFacet (#674)
    * If key is None set it to 0.
    
    * Changed test data created_date for repo to 1970-01-01.
    
    * Added function to test DateHistogramFacet with 1970-01-01 date.
    
    * Return to original value (2014-03-03) of repo created_at.
    
    * Changed comment.

--- elasticsearch_dsl/faceted_search.py
+++ elasticsearch_dsl/faceted_search.py
@@ -141,6 +141,10 @@ class DateHistogramFacet(Facet):
 
     def get_value(self, bucket):
         if not isinstance(bucket['key'], datetime):
+            # Elasticsearch returns key=None instead of 0 for date 1970-01-01,
+            # so we need to set key to 0 to avoid TypeError exception
+            if bucket['key'] is None:
+                bucket['key'] = 0
             return datetime.utcfromtimestamp(int(bucket['key']) / 1000)
         else:
             return bucket['key']
