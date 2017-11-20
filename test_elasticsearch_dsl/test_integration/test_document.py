55a97d61754c4172528f2234bd5d09e6040aa3f0
Honza Král <honza.kral@gmail.com>
2017-11-20 18:40:17 +0100

    [6.x] document tests

--- test_elasticsearch_dsl/test_integration/test_document.py
+++ test_elasticsearch_dsl/test_integration/test_document.py
@@ -23,6 +23,10 @@ class Repository(DocType):
     description = Text(analyzer='snowball')
     tags = Keyword()
 
+    @classmethod
+    def search(cls):
+        return super(Repository, cls).search().filter('term', commit_repo='repo')
+
     class Meta:
         index = 'git'
         doc_type = 'doc'
@@ -33,9 +37,9 @@ class Commit(DocType):
     description = Text(analyzer='snowball')
 
     class Meta:
-        index = 'git'
+        index = 'flat-git'
         doc_type = 'doc'
-        mapping = Mapping('commits')
+        mapping = Mapping('doc')
 
 def test_update_object_field(write_client):
     Wiki.init()
@@ -50,14 +54,10 @@ def test_update_object_field(write_client):
     assert w.owner[0].name == 'Honza'
     assert w.owner[1].name == 'Nick'
 
-def test_parent_type_is_exposed():
-    assert Commit._doc_type.parent == 'repos'
-    assert Repository._doc_type.parent is None
-
 def test_init(write_client):
     Repository.init(index='test-git')
 
-    assert write_client.indices.exists_type(index='test-git', doc_type='repos')
+    assert write_client.indices.exists_type(index='test-git', doc_type='doc')
 
 def test_get_raises_404_on_non_existent_id(data_client):
     with raises(NotFoundError):
@@ -89,17 +89,10 @@ def test_save_with_tz_date(data_client):
     assert tzinfo.localize(datetime(2014, 5, 2, 13, 47, 19, 123456)) == first_commit.committed_date
 
 COMMIT_DOCS_WITH_MISSING = [
-    {'parent': 'elasticsearch-dsl-py', '_id': '0'},                                         # Missing
-    {'parent': 'elasticsearch-dsl-py', '_id': '3ca6e1e73a071a705b4babd2f581c91a2a3e5037'},  # Existing
-    {'parent': 'elasticsearch-dsl-py', '_id': 'f'},                                         # Missing
-    {'parent': 'elasticsearch-dsl-py', '_id': 'eb3e543323f189fd7b698e66295427204fff5755'},  # Existing
-]
-
-COMMIT_DOCS_WITH_ERRORS = [
-    '0',                                                                                    # Error
-    {'parent': 'elasticsearch-dsl-py', '_id': '3ca6e1e73a071a705b4babd2f581c91a2a3e5037'},  # Existing
-    'f',                                                                                    # Error
-    {'parent': 'elasticsearch-dsl-py', '_id': 'eb3e543323f189fd7b698e66295427204fff5755'},  # Existing
+    {'_id': '0'},                                         # Missing
+    {'_id': '3ca6e1e73a071a705b4babd2f581c91a2a3e5037'},  # Existing
+    {'_id': 'f'},                                         # Missing
+    {'_id': 'eb3e543323f189fd7b698e66295427204fff5755'},  # Existing
 ]
 
 def test_mget(data_client):
@@ -119,22 +112,6 @@ def test_mget_raises_404_when_missing_param_is_raise(data_client):
 
 def test_mget_ignores_missing_docs_when_missing_param_is_skip(data_client):
     commits = Commit.mget(COMMIT_DOCS_WITH_MISSING, missing='skip')
-    assert commits[0]._id == '3ca6e1e73a071a705b4babd2f581c91a2a3e5037'
-    assert commits[1]._id == 'eb3e543323f189fd7b698e66295427204fff5755'
-
-def test_mget_raises_404_when_error_param_is_true(data_client):
-    with raises(RequestError):
-        commits = Commit.mget(COMMIT_DOCS_WITH_ERRORS)
-
-def test_mget_returns_none_for_error_docs_when_error_param_is_false(data_client):
-    commits = Commit.mget(COMMIT_DOCS_WITH_ERRORS, raise_on_error=False)
-    assert commits[0] is None
-    assert commits[1]._id == '3ca6e1e73a071a705b4babd2f581c91a2a3e5037'
-    assert commits[2] is None
-    assert commits[3]._id == 'eb3e543323f189fd7b698e66295427204fff5755'
-
-def test_mget_error_and_missing_params_together(data_client):
-    commits = Commit.mget(COMMIT_DOCS_WITH_ERRORS, raise_on_error=False, missing='skip')
     assert commits[0]._id == '3ca6e1e73a071a705b4babd2f581c91a2a3e5037'
     assert commits[1]._id == 'eb3e543323f189fd7b698e66295427204fff5755'
 
@@ -176,7 +153,7 @@ def test_save_updates_existing_doc(data_client):
     # assert version has been updated
     assert elasticsearch_repo.meta.version == v + 1
 
-    new_repo = data_client.get(index='git', doc_type='repos', id='elasticsearch-dsl-py')
+    new_repo = data_client.get(index='git', doc_type='doc', id='elasticsearch-dsl-py')
     assert 'testing-save' == new_repo['_source']['new_field']
 
 def test_save_automatically_uses_versions(data_client):
@@ -195,16 +172,16 @@ def test_can_save_to_different_index(write_client):
     assert {
         'found': True,
         '_index': 'test-document',
-        '_type': 'repos',
+        '_type': 'doc',
         '_id': '42',
         '_version': 3,
         '_source': {'description': 'testing'},
-    } == write_client.get(index='test-document', doc_type='repos', id=42)
+    } == write_client.get(index='test-document', doc_type='doc', id=42)
 
 def test_delete(write_client):
     write_client.create(
         index='test-document',
-        doc_type='repos',
+        doc_type='doc',
         id='elasticsearch-dsl-py',
         body={'organization': 'elasticsearch', 'created_at': '2014-03-03', 'owner': {'name': 'elasticsearch'}}
     )
@@ -215,26 +192,9 @@ def test_delete(write_client):
 
     assert not write_client.exists(
         index='test-document',
-        doc_type='repos',
+        doc_type='doc',
         id='elasticsearch-dsl-py',
     )
-
-def test_delete_ignores_ttl_and_timestamp_meta(write_client):
-    write_client.create(
-        index='test-document',
-        doc_type='repos',
-        id='elasticsearch-dsl-py',
-        body={'organization': 'elasticsearch', 'created_at': '2014-03-03', 'owner': {'name': 'elasticsearch'}},
-        ttl='1d',
-        timestamp=datetime.now()
-    )
-
-    test_repo = Repository(meta={'id': 'elasticsearch-dsl-py'})
-    test_repo.meta.index = 'test-document'
-    test_repo.meta.ttl = '1d'
-    test_repo.meta.timestamp = datetime.now()
-    test_repo.delete()
-
 
 def test_search(data_client):
     assert Repository.search().count() == 1
@@ -246,16 +206,6 @@ def test_search_returns_proper_doc_classes(data_client):
 
     assert isinstance(elasticsearch_repo, Repository)
     assert elasticsearch_repo.owner.name == 'elasticsearch'
-
-def test_parent_value(data_client):
-    s = Commit.search()
-    s = s.filter('term', _id='3ca6e1e73a071a705b4babd2f581c91a2a3e5037')
-    results = s.execute()
-
-    commit = results.hits[0]
-    assert 'elasticsearch-dsl-py' == commit.meta.parent
-    assert ['eb3e543323f189fd7b698e66295427204fff5755'] == commit.parent_shas
-
 
 def test_refresh_mapping(data_client):
     class Commit(DocType):
