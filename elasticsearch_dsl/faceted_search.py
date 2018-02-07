8a6faf552ae29503000dc4190d807d8f747a4f7f
konstin <konstin@mailbox.org>
2018-02-07 20:08:27 +0100

    Extend docsctring on FacetedSearch.search I found this method very confusing when I got started with FacetedSearch

--- elasticsearch_dsl/faceted_search.py
+++ elasticsearch_dsl/faceted_search.py
@@ -284,8 +284,10 @@ class FacetedSearch(object):
         self._filters[name] = f
 
     def search(self):
-        """
-        Construct the Search object.
+        """ Returns the base Search object to which the facets are added.
+
+        You can customize the query by overriding this method and returning and
+        modified search object.
         """
         s = Search(doc_type=self.doc_types, index=self.index, using=self.using)
         return s.response_class(FacetedResponse)
