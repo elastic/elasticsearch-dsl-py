e0402a5889d3d7f9a17299d67f6ade3718b27bb2
Craig Davies <craig.davies@cfpb.gov>
2020-09-17 12:12:59 -0400

    Import elasticsearch7 serializer

--- elasticsearch_dsl/serializer.py
+++ elasticsearch_dsl/serializer.py
@@ -15,7 +15,7 @@
 #  specific language governing permissions and limitations
 #  under the License.
 
-from elasticsearch.serializer import JSONSerializer
+from elasticsearch7.serializer import JSONSerializer
 
 from .utils import AttrList
 
