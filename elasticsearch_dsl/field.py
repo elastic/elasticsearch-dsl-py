4b308268884ecae76143ed04e8e5e755a00b02b3
Seth Michael Larson <seth.larson@elastic.co>
2020-12-02 13:10:35 -0600

    [7.x] Add support for 'constant_keyword'

--- elasticsearch_dsl/field.py
+++ elasticsearch_dsl/field.py
@@ -316,6 +316,10 @@ class Keyword(Field):
     name = "keyword"
 
 
+class ConstantKeyword(Keyword):
+    name = "constant_keyword"
+
+
 class Boolean(Field):
     name = "boolean"
     _coerce = True
