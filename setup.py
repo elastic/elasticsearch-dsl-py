7baaa83b430c633284f3e34cef8e73994991fde0
Quentin Pradet <quentin.pradet@elastic.co>
2023-09-07 16:25:25 +0400

    Bump main to 8.9.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from os.path import dirname, join
 
 from setuptools import find_packages, setup
 
-VERSION = (8, 0, 0)
+VERSION = (8, 9, 0)
 __version__ = VERSION
 __versionstr__ = ".".join(map(str, VERSION))
 
